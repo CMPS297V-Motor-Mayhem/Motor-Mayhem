b88531b1c9fdb2044429d1899bab56e7b48661db
elmarwanos <marwanbassam13@gmail.com>
2021-05-18 15:09:56 +0300

    Handle pause in GameStateController directly

---
+++ Assets/Scripts/Game State Behaviors/GameStateController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class GameStateController : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Pause Control
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Esc");
+            //if game is paused, unpause it
+            if (animator.GetBool("isPaused").Equals(true))
+            {
+                animator.SetBool("isPaused", false);
+                GameEvents.UnPauseEvent.Invoke();
+            }
+            //if game is unpaused, pause it
+            else
+            {
+                animator.SetBool("isPaused", true);
+                GameEvents.PauseEvent.Invoke();
+            }
+        }
+    }
+}
