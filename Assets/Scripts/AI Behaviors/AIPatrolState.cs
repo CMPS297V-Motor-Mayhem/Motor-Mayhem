8b3957a630c663d1c7e7cd26237167ae65c91255
elmarwanos <marwanbassam13@gmail.com>
2021-05-18 16:46:22 +0300

    Removed debug

--- Assets/Scripts/AI Behaviors/AIPatrolState.cs
+++ Assets/Scripts/AI Behaviors/AIPatrolState.cs
@@ -7,7 +7,6 @@ public class AIPatrolState : StateMachineBehaviour
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         car = animator.gameObject.GetComponent<AIPatrol>();
-        Debug.Log("Entered Patrol State: " + animator.gameObject.name);
     }
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
