8a081c1e3ceae708344615ad3f8182c7abc39784
Dhia Eddine Nini <dxn00@mail.aub.edu>
2021-05-19 21:40:52 +0100

    Changes in Scripts

--- Assets/Scripts/CarControl.cs
+++ Assets/Scripts/CarControl.cs
@@ -7,43 +7,40 @@ public class CarControl : MonoBehaviour
 {
     public long speed = 1500;
     public float maxRotationAngle = 15.0f;
-    private GameUIManager gameUIManager;
-
+    
     public List<Collider> throttleWheels;
     public List<Collider> steeringWheels;
+
+    // helper variables:
     private float BoostcdTime = 0.0f;
     private float SheildcdTime = 0.0f;
     private float cd = 5.0f;
+    
+    private GameUIManager gameUIManager;
+    private Abilities abilities;
 
     private void Start()
     {
         gameUIManager = GameObject.Find("UIManager").GetComponent<GameUIManager>();
-    }
-
-    private void Update()
-    {
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)) && Time.time > BoostcdTime)
-        {
-            GetComponent<Abilities>().Boost();
-            BoostcdTime = Time.time + cd;
-            StartCoroutine(gameUIManager.DisplayAbilityCooldown(gameUIManager.boostUIImage, 5, Ability.Boost));
-        }
-
-        if ((Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift) || Input.GetKeyDown(KeyCode.Mouse1)) && Time.time > SheildcdTime)
-        {
-            StartCoroutine(GetComponent<Abilities>().Shield());
-            SheildcdTime = Time.time + cd;
-            StartCoroutine(gameUIManager.DisplayAbilityCooldown(gameUIManager.shieldUIImage, 5, Ability.Shield));
-        }
+        abilities = GetComponent<Abilities>();
     }
 
     private void FixedUpdate()
     {
-        Throttle();
-        Steer();
+        HandleInput();
     }
 
-    private void Throttle()
+    // Helper Function:
+
+    private void HandleInput()
+    {
+        HandleThrottleInput();
+        HandleSteerInput();
+        HandleBoostInput();
+        HandleShieldInput();
+    }
+
+    private void HandleThrottleInput()
     {
         float dy = Input.GetAxis("Vertical");
         foreach (WheelCollider wheel in throttleWheels)
@@ -52,7 +49,7 @@ public class CarControl : MonoBehaviour
         }
     }
 
-    private void Steer()
+    private void HandleSteerInput()
     {
         float dx = Input.GetAxis("Horizontal");
         foreach (WheelCollider wheel in steeringWheels)
@@ -61,6 +58,28 @@ public class CarControl : MonoBehaviour
 
             // rotate wheel models:
             wheel.transform.localEulerAngles = new Vector3(0, dx * maxRotationAngle, 0);
+        }
+    }
+
+    private void HandleBoostInput()
+    {
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)) && Time.time > BoostcdTime)
+        {
+            abilities.Boost();
+            BoostcdTime = Time.time + cd;
+            //StartCoroutine(gameUIManager.DisplayAbilityCooldown(gameUIManager.boostUIImage, 5, Ability.Boost));
+            GameEvents.BoostEvent.Invoke(abilities.boostCooldown);
+        }
+    }
+
+    private void HandleShieldInput()
+    {
+        if ((Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift) || Input.GetKeyDown(KeyCode.Mouse1)) && Time.time > SheildcdTime)
+        {
+            StartCoroutine(abilities.Shield());
+            SheildcdTime = Time.time + cd;
+            //StartCoroutine(gameUIManager.DisplayAbilityCooldown(gameUIManager.shieldUIImage, 5, Ability.Shield));
+            GameEvents.BoostEvent.Invoke(abilities.shieldDuration);
         }
     }
 }
