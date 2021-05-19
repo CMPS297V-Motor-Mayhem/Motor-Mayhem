da8213a0810a8788123a9d8f3b64c38a530cbebb
elmarwanos <marwanbassam13@gmail.com>
2021-05-19 17:41:45 +0300

    Update Car remaining and Car knocked off & Update game win invoke

--- Assets/Scripts/OutOfBounds.cs
+++ Assets/Scripts/OutOfBounds.cs
@@ -1,23 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OutOfBounds : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        //Destroy any car that touches the lava
         if (!collision.gameObject.CompareTag("Map"))
         {
+            //Destroy any car that touches the lava
             Destroy(collision.gameObject);
+
+            //Lose Game if player falls off
             if (collision.gameObject.name.Equals("CarPlayer"))
             {
                 //if the player dies -> Game over
                 GameEvents.GameLoseEvent.Invoke();
             }
+            //Invoke KnockedOff event if agent falls off
+            else
+            {
+                GameEvents.KnockedOffCarEvent.Invoke();
+            }
         }
     }
-    
+
     private void OnCollisionStay(Collision collision)
     {
         if (!collision.gameObject.CompareTag("Map"))
